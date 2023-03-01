683ac85e640b305b83f7c1a845a9293ec0bd81c1
Goldenfreddy0703 <jenkins30661@gmail.com>
2023-02-28 22:58:55 -0500

    Rawr

---
+++ repo/plugin.video.otaku/resources/lib/pages/zoro.py
@@ -0,0 +1,68 @@
+import itertools
+import pickle
+from functools import partial
+
+from resources.lib.ui import database
+from resources.lib.ui.BrowserBase import BrowserBase
+from resources.lib.indexers import consumet
+
+
+class sources(BrowserBase):
+    def get_sources(self, anilist_id, episode, get_backup):
+        all_results = []
+        show = database.get_show(anilist_id)
+        kodi_meta = pickle.loads(show.get('kodi_meta'))
+        title = kodi_meta.get('ename') or kodi_meta.get('name')
+        title = self._clean_title(title)
+        title = '{0} Ep-{1}'.format(title, episode)
+
+        for x in ['sub', 'dub']:
+            r = database.get(
+                consumet.CONSUMETAPI().get_sources,
+                8,
+                anilist_id,
+                episode,
+                'zoro',
+                x
+            )
+
+            if r and r.get('sources'):
+                srcs = r.get('sources')
+                subs = r.get('subtitles')
+                if subs:
+                    subs = [x for x in subs if x.get('lang') != 'Thumbnails']
+                mapfunc = partial(self._process_ap, title=title, lang=2 if x == 'dub' else 0, subs=subs)
+                results = list(map(mapfunc, srcs))
+                results = list(itertools.chain(*results))
+                all_results += results
+        return all_results
+
+    def _process_ap(self, item, title='', lang=0, subs=[]):
+        sources = []
+        slink = item.get('url')
+        qual = item.get('quality')
+        if qual.endswith('p'):
+            qual = int(qual[:-1])
+            if qual < 577:
+                quality = 'NA'
+            elif qual < 721:
+                quality = '720p'
+            elif qual < 1081:
+                quality = '1080p'
+            else:
+                quality = '4K'
+
+            source = {
+                'release_title': title,
+                'hash': slink,
+                'type': 'direct',
+                'quality': quality,
+                'debrid_provider': '',
+                'provider': 'zoro',
+                'size': 'NA',
+                'info': ['DUB' if lang == 2 else 'SUB', 'HLS' if item.get('isM3U8') else ''],
+                'lang': lang,
+                'subs': subs
+            }
+            sources.append(source)
+        return sources
---
+++ repo/plugin.video.otaku/resources/lib/ui/utils.py
@@ -0,0 +1,80 @@
+import os
+import random
+import re
+from resources.lib.ui import control, client
+from six import StringIO
+from kodi_six import xbmcvfs
+
+
+def allocate_item(name, url, is_dir=False, image='', info='', fanart=None, poster=None, cast=[], landscape=None, banner=None, clearart=None, clearlogo=None):
+    new_res = {}
+    new_res['is_dir'] = is_dir
+    if image and '/' not in image:
+        image = os.path.join(control.artPath(), image)
+    if fanart:
+        if isinstance(fanart, list):
+            fanart = random.choice(fanart)
+        if '/' not in fanart:
+            fanart = os.path.join(control.artPath(), fanart)
+    if poster and '/' not in poster:
+        poster = os.path.join(control.artPath(), poster)
+    new_res['image'] = {
+        'poster': poster or image,
+        'icon': image,
+        'thumb': image,
+        'fanart': fanart,
+        'landscape': landscape,
+        'banner': banner,
+        'clearart': clearart,
+        'clearlogo': clearlogo
+    }
+    new_res['name'] = name
+    new_res['url'] = url
+    new_res['info'] = info
+    new_res['cast'] = cast
+    return new_res
+
+
+def get_sub(sub_url, sub_lang):
+    content = client.request(sub_url)
+    subtitle = control.TRANSLATEPATH('special://temp/')
+    fname = 'TemporarySubs.{0}.srt'.format(sub_lang)
+    fpath = os.path.join(subtitle, fname)
+    if sub_url.endswith('.vtt'):
+        if control._kodiver > 19.8:
+            fname = fname.replace('.srt', '.vtt')
+            fpath = fpath.replace('.srt', '.vtt')
+        else:
+            replacement = re.sub(r'([\d]+)\.([\d]+)', r'\1,\2', content)
+            replacement = re.sub(r'WEBVTT\n\n', '', replacement)
+            replacement = re.sub(r'^\d+\n', '', replacement)
+            replacement = re.sub(r'\n\d+\n', '\n', replacement)
+            replacement = StringIO(replacement)
+            idx = 1
+            content = ''
+            for line in replacement:
+                if '-->' in line:
+                    if len(line.split(' --> ')[0]) < 12:
+                        line = re.sub(
+                            r'([\d]+):([\d]+),([\d]+)', r'00:\1:\2,\3', line)
+                    content += '%s\n%s' % (idx, line)
+                    idx += 1
+                else:
+                    content += line
+
+    if control.PY3:
+        with open(fpath, 'w', encoding='utf-8') as f:
+            f.write(content)
+    else:
+        with open(fpath, 'w') as f:
+            f.write(content.encode('utf8'))
+
+    return 'special://temp/' + fname
+
+
+def del_subs():
+    dirs, files = xbmcvfs.listdir('special://temp/')
+    for fname in files:
+        if fname.startswith('TemporarySubs'):
+            xbmcvfs.delete('special://temp/' + fname)
+    return
